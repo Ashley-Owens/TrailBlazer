ab0bdb5e96b39dbf6e087ed705d9c521a9e52bca
Zaki Ahmed <ZakiAhmed@alumni.usc.edu>
2020-11-08 20:22:51 -0500

    push from Z / R final

--- comments.py
+++ comments.py
@@ -1,11 +1,11 @@
 """
 
 Home page
-    DONE:
+    DONE
         •	Fix search button on home page
 
 Profile page
-    TODO
+    DONE
         •	Add calculate button to profile page
         •	Show result of fitness level estimation and describe what it means and how it is calculated
 
@@ -22,15 +22,18 @@ Gear page
     DONE
         •	Fix nav to be consistent with other pages
         •	Make links work
-    TODO
+    DONE
         •	Add description for why each item is recommended
 
 Product backlog (create ClickUp tasks)
     TODO 
         •	Feature 3: Scenario 2 of 3
         •	Feature 3: Scenario 3 of 3
-        •	Have profile information persist on profile page
         •	Backend/data work
+        •	Backend calculation of fitness level
+    DONE
+        •	Have profile information persist on profile page
+
 
 
 """
