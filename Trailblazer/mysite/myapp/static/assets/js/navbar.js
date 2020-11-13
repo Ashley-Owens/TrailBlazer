47a07cd077d0dab95b96aafa03a762fbb1969cc0
Zaki Ahmed <ZakiAhmed@alumni.usc.edu>
2020-11-13 02:28:18 -0500

    push from ZA, removed Django

--- Trailblazer/mysite/myapp/static/assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    document.addEventListener("DOMContentLoaded", () => {
-        const navbar = document.getElementById("navbar");
-        const content = document.getElementById("content");
-
-        content.style.paddingTop = navbar.getBoundingClientRect().height + "px";  
-    })
-})();
