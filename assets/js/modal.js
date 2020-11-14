6e556dc4dfa0b99ebfa7a504e7536c354afff96a
booleancube <ashleyowenspt@gmail.com>
2020-11-14 10:32:38 -0700

    Fixed bug when toggling modal switch on different pages

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,22 +4,25 @@ function disableRadios() {
     $('[name="modalRadio"]').prop('disabled', true);
 }
 
+
 // Tracks toggle button state to enable or disable radio buttons
 $(document).ready(function() {
     $("#customSwitches").click(function() {  
-        if ($('[name="modalRadio"]').prop("disabled") === true) {
-            $('[name="modalRadio"]').prop("disabled", false);   
+
+        if ($("#customSwitches").prop('checked') === true) {
+            $('[name="modalRadio"]').prop("disabled", false);
         } else {
             disableRadios();
         }
     });
 });
 
+
 // When either modal close buttons are used, modal is reset to default state
 $(document).ready(function(){
     $("[name='closeModal']").click (function () {
-        $("#customSwitches").prop('checked', false);
         disableRadios();
+        $("#customSwitches").prop('checked', false);  
     })
 });
 
