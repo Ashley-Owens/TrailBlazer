c4772e7b26a2ef22800cf28249c5459e8dfa28fb
booleancube <ashleyowenspt@gmail.com>
2020-11-13 23:29:25 -0700

    added 2nd modal close button functionality

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,32 +1,23 @@
 // Disables and clears radio buttons, preventing user selection
 function disableRadios() {
-    $('input[name="modalRadio"]').prop('checked', false);
-    $("#formCheck-1").prop('disabled', true);
-    $("#formCheck-2").prop('disabled', true);
-    $("#formCheck-3").prop('disabled', true);
-}
-
-// Enables radios buttons, allowing user selection
-function enableRadios() {
-    $("#formCheck-1").prop("disabled", false);
-    $("#formCheck-2").prop('disabled', false);
-    $("#formCheck-3").prop('disabled', false);
+    $('[name="modalRadio"]').prop('checked', false);
+    $('[name="modalRadio"]').prop('disabled', true);
 }
 
 // Tracks toggle button state to enable or disable radio buttons
 $(document).ready(function() {
     $("#customSwitches").click(function() {  
-        if ($("#formCheck-1").prop("disabled") === true) {
-            enableRadios();    
+        if ($('[name="modalRadio"]').prop("disabled") === true) {
+            $('[name="modalRadio"]').prop("disabled", false);   
         } else {
             disableRadios();
         }
     });
 });
 
-// When the "close" button is used, modal is reset to default state
+// When either modal close buttons are used, modal is reset to default state
 $(document).ready(function(){
-    $('#closeModal').click (function () {
+    $("[name='closeModal']").click (function () {
         $("#customSwitches").prop('checked', false);
         disableRadios();
     })
