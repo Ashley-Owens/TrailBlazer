b32bc7cb38f2fd771fb2dfa6d7baaf95e25e4ff5
booleancube <ashleyowenspt@gmail.com>
2020-11-13 23:00:59 -0700

    Added close button functionality and fixed toggle error

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,5 +1,6 @@
-// Disables radios buttons, preventing user selection
+// Disables and clears radio buttons, preventing user selection
 function disableRadios() {
+    $('input[name="modalRadio"]').prop('checked', false);
     $("#formCheck-1").prop('disabled', true);
     $("#formCheck-2").prop('disabled', true);
     $("#formCheck-3").prop('disabled', true);
@@ -14,22 +15,20 @@ function enableRadios() {
 
 // Tracks toggle button state to enable or disable radio buttons
 $(document).ready(function() {
-
-    disableRadios();
-    var radioState = false;
-
-    $('input[type="checkbox"]').click(function() {
-        if($(this).prop('id') === "customSwitches") {
-            
-            if (radioState === true) {
-                document.querySelector('input[name="modalRadio"]:checked').checked = false;
-                disableRadios();
-                radioState = false;
-                
-            } else {
-                enableRadios();
-                radioState = true;
-            }
-        }	 
+    $("#customSwitches").click(function() {  
+        if ($("#formCheck-1").prop("disabled") === true) {
+            enableRadios();    
+        } else {
+            disableRadios();
+        }
     });
 });
+
+// When the "close" button is used, modal is reset to default state
+$(document).ready(function(){
+    $('#closeModal').click (function () {
+        $("#customSwitches").prop('checked', false);
+        disableRadios();
+    })
+});
+
