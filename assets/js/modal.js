6bb85a6af1d8d8c54c02e5b6b4534f35ecfc9c35
booleancube <ashleyowenspt@gmail.com>
2020-11-15 12:32:55 -0700

    updated branch to current working files

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,24 @@
+
 var toggle = document.querySelector("#customSwitches");
 var radios = document.getElementsByName("modalRadio");
+var filterButton = document.getElementsByName("filterSearch");
 var saveButton = document.getElementById("Save");
+myStorage = window.localStorage;
 
+
+// Checks for user settings and updates modal as needed upon modal opening.
+$(document).ready(function(){
+    $(filterButton).click (function () {
+
+        if (myStorage.getItem("userSelection")) {
+            var selectionID = document.getElementById(myStorage.getItem("userSelection"));
+            $(radios).prop("disabled", false);
+            $(selectionID).prop("checked", true);
+            $(toggle).prop('checked', true);
+        }
+    })
+});
+        
 
 // Disables and clears radio buttons, preventing user selection
 function disableRadios() {
@@ -10,13 +27,20 @@ function disableRadios() {
 }
 
 
-// Tracks toggle button state to enable or disable radio buttons
+// Removes user input from local storage
+function clearStorage() {
+    myStorage.removeItem("userSelection");
+}
+
+
+// Tracks toggle button state to enable or disable radio buttons and clear storage if needed.
 $(document).ready(function() {
     $(toggle).click(function() {  
         if ($(toggle).prop('checked') === true) {
             $(radios).prop("disabled", false);
         } else {
             disableRadios();
+            clearStorage();
         }
     });
 });
@@ -25,41 +49,41 @@ $(document).ready(function() {
 // When either modal close buttons are clicked, modal is reset to default state.
 $(document).ready(function(){
     $("[name='closeModal']").click (function () {
+        $(toggle).prop('checked', false);
         disableRadios();
-        $(toggle).prop('checked', false);  
+        clearStorage();
     })
 });
 
 
-// TODO
-// When save button clicked, stores user input and saves settings.
+// When save button is clicked, user input stored if needed and modal settings saved.
 $(document).ready(function(){
     $(saveButton).click (function () {
-        input = getSelection();
-        if (input !== undefined) {
+
+        if ($(toggle).prop('checked') === true) {
+            input = getSelection();
             saveSettings(input);
         } 
     })
 });
 
 
-// Obtains the user's radio button selection.
+// Obtains the selected radio button id#.
 function getSelection() {
-    var selection;
-
     for (var i=0; i < radios.length; i++) {
         if (radios[i].checked === true) {
-            selection = radios[i].value;
+            input = radios[i].id;
         }
     }
-    return selection;
+    return input;
 }
 
 
-// TODO
-// Saves modal settings so they persist across both web pages.
+// Saves user input to local storage so that settings persist across both web pages.
 function saveSettings (input) {
-    if (input !== undefined){
-        var x;
+    if (input !== undefined && window.localStorage){ 
+        myStorage.setItem("userSelection", input);  
     }
 }
+
+
