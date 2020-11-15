b4b6fa2dc452bf391a2eb9fe76c516defcf270fb
booleancube <ashleyowenspt@gmail.com>
2020-11-14 23:26:46 -0700

    Save button stores user's input

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,16 +1,20 @@
+var toggle = document.querySelector("#customSwitches");
+var radios = document.getElementsByName("modalRadio");
+var saveButton = document.getElementById("Save");
+
+
 // Disables and clears radio buttons, preventing user selection
 function disableRadios() {
-    $('[name="modalRadio"]').prop('checked', false);
-    $('[name="modalRadio"]').prop('disabled', true);
+    $(radios).prop('checked', false);
+    $(radios).prop('disabled', true);
 }
 
 
 // Tracks toggle button state to enable or disable radio buttons
 $(document).ready(function() {
-    $("#customSwitches").click(function() {  
-
-        if ($("#customSwitches").prop('checked') === true) {
-            $('[name="modalRadio"]').prop("disabled", false);
+    $(toggle).click(function() {  
+        if ($(toggle).prop('checked') === true) {
+            $(radios).prop("disabled", false);
         } else {
             disableRadios();
         }
@@ -18,11 +22,44 @@ $(document).ready(function() {
 });
 
 
-// When either modal close buttons are used, modal is reset to default state
+// When either modal close buttons are clicked, modal is reset to default state.
 $(document).ready(function(){
     $("[name='closeModal']").click (function () {
         disableRadios();
-        $("#customSwitches").prop('checked', false);  
+        $(toggle).prop('checked', false);  
     })
 });
 
+
+// TODO
+// When save button clicked, stores user input and saves settings.
+$(document).ready(function(){
+    $(saveButton).click (function () {
+        input = getSelection();
+        if (input !== undefined) {
+            saveSettings(input);
+        } 
+    })
+});
+
+
+// Obtains the user's radio button selection.
+function getSelection() {
+    var selection;
+
+    for (var i=0; i < radios.length; i++) {
+        if (radios[i].checked === true) {
+            selection = radios[i].value;
+        }
+    }
+    return selection;
+}
+
+
+// TODO
+// Saves modal settings so they persist across both web pages.
+function saveSettings (input) {
+    if (input !== undefined){
+        var x;
+    }
+}
