c2f6dbd8c08a1453b27df79640b393969b08f138
liu-ronny <54465266+liu-ronny@users.noreply.github.com>
2020-11-22 20:45:12 -0800

    Fix code smell: speculative generality

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -7,38 +7,30 @@
         const input = $(this);
         input.val("");
 
-        if (window.localStorage) {
-          window.localStorage.removeItem(input.attr("name"));
-        }
+        window.localStorage.removeItem(input.attr("name"));
       });
 
-      if (window.localStorage) {
-        window.localStorage.removeItem("fitnessLevel");
-        $("#fitness-level").text(
-          `Please enter in your profile information above to calculate your fitness level`
-        );
-      }
+      window.localStorage.removeItem("fitnessLevel");
+      $("#fitness-level").text(
+        `Please enter in your profile information above to calculate your fitness level`
+      );
     });
 
-    if (window.localStorage) {
-      $(".profile-input").each(function () {
-        const input = $(this);
-        input.val(window.localStorage.getItem(input.attr("name")));
-      });
+    $(".profile-input").each(function () {
+      const input = $(this);
+      input.val(window.localStorage.getItem(input.attr("name")));
+    });
 
-      const fitnessLevel = window.localStorage.getItem("fitnessLevel");
+    const fitnessLevel = window.localStorage.getItem("fitnessLevel");
 
-      if (fitnessLevel) {
-        $("#fitness-level")
-          .text(`Your calculated fitness level is `)
-          .append(
-            `<span class="text-primary text-bold">${fitnessLevel}</span>`
-          );
-      } else {
-        $("#fitness-level").text(
-          `Please enter in your profile information above to calculate your fitness level`
-        );
-      }
+    if (fitnessLevel) {
+      $("#fitness-level")
+        .text(`Your calculated fitness level is `)
+        .append(`<span class="text-primary text-bold">${fitnessLevel}</span>`);
+    } else {
+      $("#fitness-level").text(
+        `Please enter in your profile information above to calculate your fitness level`
+      );
     }
   });
 })();
